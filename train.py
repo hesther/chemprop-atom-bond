b3ebbe6361fc555337c6fc3244d08f4d8e4095f1
Yanfei Guan <yanfeig@mit.edu>
2020-02-10 12:03:54 -0500

    add metric to train

--- train.py
+++ train.py
@@ -5,14 +5,16 @@ from chemprop.train import cross_validate, run_training
 from chemprop.utils import create_logger
 import pickle
 
+import os
+
 if __name__ == '__main__':
     args = parse_train_args()
     args.explicit_Hs = True
     args.data_path = 'chembl_results.pickle.gz'
     logger = create_logger(name='train', save_dir=args.save_dir, quiet=args.quiet)
     test_avg_score, test_preds, test_smiles = run_training(args, logger)
-    with open('{}_test_preds.pickle'.format(args.output), 'wb') as preds:
+    with open(os.path.join(args.save_dir, 'test_preds.pickle'), 'wb') as preds:
         pickle.dump(test_preds, preds)
 
-    with open('{}_test_smiles.pickle'.format(args.output), 'wb') as smiles:
+    with open(os.path.join(args.save_dir, 'test_smiles.pickle'), 'wb') as smiles:
         pickle.dump(test_smiles, smiles)
