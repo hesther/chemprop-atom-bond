52bfbfbc0c4543cb0215dc491e4cf831094c96c7
Yanfei Guan <yanfeig@mit.edu>
2020-03-03 15:34:30 -0500

    predict

--- predict.py
+++ predict.py
@@ -3,9 +3,14 @@
 from chemprop.parsing import parse_predict_args
 from chemprop.train import make_predictions
 
+import pandas as pd
+
 if __name__ == '__main__':
     args = parse_predict_args()
-    test_preds, test_smiles = make_predictions(args)
+
+    test_df = pd.read_csv(args.test_path, index_col=0)
+    smiles = test_df.smiles.values
+    test_preds, test_smiles = make_predictions(args, smiles=smiles)
 
     with open(os.path.join(args.save_dir, 'preds.pickle'), 'wb') as preds:
         pickle.dump(test_preds, preds)
